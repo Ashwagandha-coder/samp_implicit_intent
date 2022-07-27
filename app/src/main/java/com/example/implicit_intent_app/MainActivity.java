716e73c288b4397a7a35f9be195b0680187cf67c
Ashwaganha-cader <max.cacz@yandex.ru>
2022-07-27 18:38:24 +0500

    add toast

--- app/src/main/java/com/example/implicit_intent_app/MainActivity.java
+++ app/src/main/java/com/example/implicit_intent_app/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.implicit_intent_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickOpenWebpageButton(View view) {
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(MainActivity.this, "Button was clicked", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+    }
+
+    public void onClickOpenMapButton(View view) {
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(MainActivity.this, "Button was clicked", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+    }
+
+    public void yourOwnImplicitIntent(View view) {
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(MainActivity.this, "Button was clicked", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
     }
 }
